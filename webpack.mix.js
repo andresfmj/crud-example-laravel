8845aba6953a1b65f12b83680448ceec9a57c89c
Andres Fernando <andres.fernando08@gmail.com>
2020-03-13 01:09:09 -0500

    se eliminan los sourcemaps en ambiente de produccion

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,10 @@ const mix = require('laravel-mix');
 
 mix.react('resources/js/index.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+   mix.version()
+} else {
+   mix.sourceMaps()
+}
+
