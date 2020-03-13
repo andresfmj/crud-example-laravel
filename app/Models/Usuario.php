26d2ed76daaf5d54e406a83ccadf4dcb80590249
Andres Fernando <andres.fernando08@gmail.com>
2020-03-13 01:02:21 -0500

    creado modelos y configurado el modelo de usuario en auth

---
+++ app/Models/Usuario.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Usuario extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nombre', 'email', 'password', 'api_token'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = ['password'];
+
+}
