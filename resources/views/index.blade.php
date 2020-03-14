5784a73c1ae87db1f523c06385bb15109484a1da
Andres Fernando <andres.fernando08@gmail.com>
2020-03-14 02:24:41 -0500

    el index blade usa ahora una de las variables de entorno para mostrar el titulo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>High Developer - Challenge</title>
+    <title>{{ config('app.name') }}</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
--- resources/views/react/index.blade.php
+++ resources/views/react/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>High Developer - Challenge</title>
+        <title>{{ config('app.name') }}</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>
