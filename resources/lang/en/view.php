76ce57e19ce43713e1f49bdaed53ee67bb295e98
Andres Fernando <andres.fernando08@gmail.com>
2020-03-14 02:22:57 -0500

    agregado nuevo string de idioma

--- resources/lang/en/view.php
+++ resources/lang/en/view.php
@@ -2,5 +2,6 @@
 
 return [
     'duplicate_email' => 'This email address is already been taken',
-    'new_user_created' => 'The user has been registered correctly'
+    'new_user_created' => 'The user has been registered correctly',
+    'user_updated' => 'User updated correctly'
 ];
