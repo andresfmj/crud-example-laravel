8d672eead0e95885e96fbab5cda77e35a9e1955f
Andres Fernando <andres.fernando08@gmail.com>
2020-03-14 22:19:38 -0500

    agregado strings en ingles y se agregaron archivos de idioma en español.

--- resources/lang/en/view.php
+++ resources/lang/en/view.php
@@ -3,5 +3,11 @@
 return [
     'duplicate_email' => 'This email address is already been taken',
     'new_user_created' => 'The user has been registered correctly',
-    'user_updated' => 'User updated correctly'
+    'user_updated' => 'User updated correctly',
+    'user_removed' => 'User has been removed from the database.',
+    'cannot_remove_user' => 'It cannot remove an user that is currently logged in',
+    'new_cita_created' => 'New appointment has been added',
+    'dates_overlap' => 'There is already an appointment in this date range',
+    'cita_removed' => 'Appointment was removed',
+    'cita_modified' => 'Appointment updated'
 ];
---
+++ resources/lang/es/view.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'duplicate_email' => 'Este email ya ha sido registrado antes',
+    'new_user_created' => 'El usuario ha sido registrado correctamente',
+    'user_updated' => 'Usuario modificado',
+    'user_removed' => 'Usuario eliminado.',
+    'cannot_remove_user' => 'No se puede eliminar un usuario actualmente autenticado',
+    'new_cita_created' => 'Nueva cita añadida',
+    'dates_overlap' => 'Ya existe una cita en este rango de fechas',
+    'cita_removed' => 'Cita eliminada',
+    'cita_modified' => 'Cita modificada'
+];
